4f6c1deef8df4dd207acae4c698805ff03e83a32
Ash530888 <aishah.s.qureshi@gmail.com>
2022-03-30 21:10:10 +0100

    Commented AdministrativeAccount.java

--- src/main/java/com/example/mentormatching/model/AdministrativeAccount.java
+++ src/main/java/com/example/mentormatching/model/AdministrativeAccount.java
@@ -5,10 +5,26 @@
 package com.company.src.main.java.com.example.mentormatching.model;
 
 public class AdministrativeAccount extends Account {
+
+    /*
+     * Constructor
+     *
+     * @param  String  email
+     * @param  String  password
+     * @param  String  pin
+     * @return void
+     */
     public AdministrativeAccount(String email, String password, String pin) {
         super(email, password, pin);
     }
 
+    /*
+     * Constructor
+     *
+     * @param  String  email
+     * @param  String  password
+     * @return void
+     */
     public AdministrativeAccount(String email, String password) {
         super(email, password);
     }
