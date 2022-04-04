c762e870a57beb5cf730080778679444347448d0
Ash530888 <aishah.s.qureshi@gmail.com>
2022-04-04 12:03:19 +0100

    Pushing Controllers

--- src/main/java/com/example/mentormatching/controller/LoginController.java
+++ src/main/java/com/example/mentormatching/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
 
+
     private userService userService = new userService();
 
     @GetMapping("/login")
--- src/main/java/com/example/mentormatching/controller/chatController.java
+++ src/main/java/com/example/mentormatching/controller/chatController.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class chatController {
 
 
+
     @GetMapping("/chat")
     public String showChat(Model model){
 
