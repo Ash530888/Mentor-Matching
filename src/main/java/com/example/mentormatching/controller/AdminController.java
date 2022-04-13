5f90ad5f0865eaf7226f86996019a8ebe3ac24ae
fatin <fatin1235674556@gmail.com>
2022-04-13 01:18:43 +0100

    finally fixed suggested mentor

--- src/main/java/com/example/mentormatching/controller/AdminController.java
+++ src/main/java/com/example/mentormatching/controller/AdminController.java
@@ -7,6 +7,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MenteeSelected{
+
+    public String[] menteeSelected = {"","",""};
+
+    public String[] getMenteeSelected() {
+        return menteeSelected;
+    }
+
+    public void setMenteeSelected(String[] menteeSelected) {
+        this.menteeSelected = menteeSelected;
+    }
+}
+
+class  MentorSelected{
+
+    public String mentor;
+
+    public String getMentor() {
+        return mentor;
+    }
+
+    public void setMentor(String mentor) {
+        this.mentor = mentor;
+    }
+}
 
 @Controller
 public class AdminController {
@@ -20,9 +52,21 @@ public class AdminController {
     @GetMapping("/suggestMatch")
     public String getSuggestPage(Model model){
 
-        model.addAttribute("Mentor", mentorService.getMentor());
-        model.addAttribute("unmatchedMentee", menteeService.getMentee());
+        String[] menteeSelected = {"","","",""};
 
-        return "suggestMatch";
+        model.addAttribute("mentors", mentorService.getMentor());
+        model.addAttribute("unmatchedMentee", menteeService.getNonConnectedMentees());
+        model.addAttribute("menteeSelected", new MenteeSelected());
+        model.addAttribute("mentorSelected", new MentorSelected());
+        return "suggestMentor";
+    }
+
+    @PostMapping("/processSuggestMatch")
+    public String processSuggestMatch(@ModelAttribute MenteeSelected menteeSelected,@ModelAttribute MentorSelected mentorSelected  ,RedirectAttributes redirectAttributes){
+        System.out.println(mentorSelected.mentor);
+        for (String a: menteeSelected.menteeSelected){
+            System.out.println(a);
+        }
+        return "home";
     }
 }
